f82026807f2aa02869bf781fd2ae6d853e48040a
ngocdu <ngocdu171@gmail.com>
2021-08-28 09:26:19 +0300

    create router update

--- server/routers/posts.js
+++ server/routers/posts.js
@@ -1,32 +1,43 @@
-import express from 'express';
-import { PostModel } from '../models/PostModel.js';
+import express from "express";
+import { PostModel } from "../models/PostModel.js";
 
 const router = express.Router();
 
-router.get('/', async(req, res) => {
-    try {
-        const posts = await PostModel.find();
-        console.log(posts);
-        res.status(200).json(posts);
-    } catch (error) {
-        res.status(500).json(error);
-    }
+router.get("/", async (req, res) => {
+  try {
+    const posts = await PostModel.find();
+    console.log(posts);
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.post('/', async (req, res) => {
-    try {
-        const newPost = req.body;
+router.post("/", async (req, res) => {
+  try {
+    const newPost = req.body;
 
-        const post = new PostModel(newPost);
-        await post.save();
-        res.status(200).json(post);
-    } catch (error) {
-        res.status(500).json(error);
-    }
-})
+    const post = new PostModel(newPost);
+    await post.save();
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
-router.get('/home', (req, res) => {
-    res.send('ahihi!');
+router.put("/home", async (req, res) => {
+  try {
+    const updatePost = req.body;
+
+    const post = await PostModel.findOneAndUpdate(
+      { _id: updatePost },
+      updatePost,
+      { new: true } //option new = true: de lay du lieu moi nhat sau khi cap nhat. ko co option nay se tu dong lau du lieu cu
+    );
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 export default router;
