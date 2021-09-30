66741fedb848dda51111eea4ec81007a14bbe88e
ngocdu <ngocdu171@gmail.com>
2021-09-30 18:22:57 +0300

    trigger action getPostsRequest

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Col, Container, Row } from "reactstrap";
 import Post from "./Post";
+import * as actions from '../../redux/actions'
 
 export default function PostList() {
+
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(actions.getPosts.getPostsRequest())
+    }, [dispatch])
+
   return (
     <Row>
       <Col xs="12" sm="6">
